0b031d84df9d3502aef769c04688ceb290cdb06a
Greg Richardson <greg.nmr@gmail.com>
2023-10-24 18:23:49 +0000

    feat: gitpod support

--- app/auth/sign-in/route.ts
+++ app/auth/sign-in/route.ts
@@ -1,14 +1,14 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url);
+  const requestUrl = new URL(request.headers.get("origin") ?? request.url);
   const formData = await request.formData();
-  const email = String(formData.get('email'));
-  const password = String(formData.get('password'));
+  const email = String(formData.get("email"));
+  const password = String(formData.get("password"));
   const supabase = createRouteHandlerClient({ cookies });
 
   const { error } = await supabase.auth.signInWithPassword({
@@ -17,6 +17,7 @@ export async function POST(request: Request) {
   });
 
   if (error) {
+    console.error(error);
     return NextResponse.redirect(
       `${requestUrl.origin}/login?error=Could not authenticate user`,
       {
--- app/auth/sign-out/route.ts
+++ app/auth/sign-out/route.ts
@@ -1,11 +1,11 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url);
+  const requestUrl = new URL(request.headers.get("origin") ?? request.url);
   const supabase = createRouteHandlerClient({ cookies });
 
   await supabase.auth.signOut();
